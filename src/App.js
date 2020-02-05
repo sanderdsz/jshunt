09aa21ff7916a3275980aa5857d182230bd6e98b
sanderdsz <sanderdsz@gmail.com>
2020-02-04 22:26:06 -0300

    response from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+
+// Api connector
+import api from './services/api'
+
+// Styles
 import './styles.css';
 
+// Components
 import Header from './components/header'
+import Main from './pages/main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Main />
       </div >
     )
   }
---
+++ src/pages/main/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import api from "../../services/api"
+
+export default class Main extends Component {
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products')
+
+        console.log(response.data.docs);
+    }
+
+
+    render() {
+        return <div>
+            <h1>Hello world, Sander.</h1>
+        </div>
+    }
+}
